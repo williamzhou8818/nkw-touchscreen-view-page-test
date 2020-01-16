e122738638a5787776d63e4f86326555130c32a0
william zhou <william.zhou8818@gmail.com>
2020-01-16 20:21:05 +1100

    Updates

---
+++ precache-manifest.c76d517d2034d0b508cde0f913dd612b.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfff483fdc1bb3f4e251a50290e00ab8",
+    "url": "/nkw-touchscreen-view-page-test/index.html"
+  },
+  {
+    "revision": "561463b8130bfd3a7d92",
+    "url": "/nkw-touchscreen-view-page-test/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "67351048a0c872182583",
+    "url": "/nkw-touchscreen-view-page-test/static/css/main.345ec415.chunk.css"
+  },
+  {
+    "revision": "561463b8130bfd3a7d92",
+    "url": "/nkw-touchscreen-view-page-test/static/js/2.efcefce8.chunk.js"
+  },
+  {
+    "revision": "67351048a0c872182583",
+    "url": "/nkw-touchscreen-view-page-test/static/js/main.31fa3b48.chunk.js"
+  },
+  {
+    "revision": "179ae557124b7437e9c8",
+    "url": "/nkw-touchscreen-view-page-test/static/js/runtime~main.f75b6a2c.js"
+  },
+  {
+    "revision": "471b3a910ed73777d6d5ce765243ab19",
+    "url": "/nkw-touchscreen-view-page-test/static/media/2-StaticAdMiddle.471b3a91.png"
+  },
+  {
+    "revision": "d40c378da20dfb990c4af3d96dcc5c08",
+    "url": "/nkw-touchscreen-view-page-test/static/media/AboutUs_Image.d40c378d.png"
+  },
+  {
+    "revision": "f2cd92ef9e72f1b694b1dbf267d5a548",
+    "url": "/nkw-touchscreen-view-page-test/static/media/AboutUs_Who_we_are_1.f2cd92ef.png"
+  },
+  {
+    "revision": "3769332d1693b4aaaa7785f04c4b130d",
+    "url": "/nkw-touchscreen-view-page-test/static/media/CategoryFooterImagesTraining.3769332d.png"
+  },
+  {
+    "revision": "8534dd898bc7913eba5a382ca7213b8f",
+    "url": "/nkw-touchscreen-view-page-test/static/media/CategoryFooterImages_Building.8534dd89.png"
+  },
+  {
+    "revision": "42c7759b05dfae1d9c834d78e3355e4a",
+    "url": "/nkw-touchscreen-view-page-test/static/media/CategoryFooterImages_Catering.42c7759b.png"
+  },
+  {
+    "revision": "0da208d6ab3b9ff2af83327f19e57bc0",
+    "url": "/nkw-touchscreen-view-page-test/static/media/CategoryFooterImages_Logistics.0da208d6.png"
+  },
+  {
+    "revision": "45f512b43a7c6a35ed0a0b7d88189be7",
+    "url": "/nkw-touchscreen-view-page-test/static/media/CateringImage.45f512b4.png"
+  },
+  {
+    "revision": "68c472ab9d49f9917f1819d7016ab02e",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_APS.68c472ab.png"
+  },
+  {
+    "revision": "1c9b635088f45626bd43f57284766074",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_BuildingConstruction.1c9b6350.png"
+  },
+  {
+    "revision": "1c9b635088f45626bd43f57284766074",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_Building_Construction.1c9b6350.png"
+  },
+  {
+    "revision": "8e936155be6a9534ea968cea4f20a8d1",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_CampManagement.8e936155.png"
+  },
+  {
+    "revision": "d2a45e30541bfefe4d65ecc3b7b48cbc",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_Catering.d2a45e30.png"
+  },
+  {
+    "revision": "4032bfc43728601f34e08bc7c74ca6b8",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_Expac.4032bfc4.png"
+  },
+  {
+    "revision": "de537a0dc91befb471a469306cc456ad",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_Fresh.de537a0d.png"
+  },
+  {
+    "revision": "1f9f4053c831135aff12847fc90fa374",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_HVC.1f9f4053.png"
+  },
+  {
+    "revision": "7a845195682dfde3947f97b23c66238e",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_HiddenValley.7a845195.png"
+  },
+  {
+    "revision": "82c12e574e6ad7bf21ca306c69457ce8",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_JetSmart.82c12e57.png"
+  },
+  {
+    "revision": "2068e8334bb3f712a71a10070fadd004",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_Logistics&Transport.2068e833.png"
+  },
+  {
+    "revision": "4c26e953e7ee4db5eaf79223f9eacfb8",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_MTO.4c26e953.png"
+  },
+  {
+    "revision": "c7cfc758e77b7cd4b9573fda7691ab1b",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_MTOTraining.c7cfc758.png"
+  },
+  {
+    "revision": "87ae1989c85d1b244426742b105869c3",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_MountainCoffee.87ae1989.png"
+  },
+  {
+    "revision": "5d593fd4f660eb923c8d985762d91872",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_NKWFresh.5d593fd4.png"
+  },
+  {
+    "revision": "7d0b734082c21395e490be17c823b5b9",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_NKW_Videos.7d0b7340.png"
+  },
+  {
+    "revision": "f714681695b97d2ffe5d091a33728b64",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_NKWcatering.f7146816.png"
+  },
+  {
+    "revision": "045251b44e500ec304bf99ed4eabe040",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_PCS.045251b4.png"
+  },
+  {
+    "revision": "bf3c929c38ad2e9df55d5d17a0ff4ee9",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_PCV.bf3c929c.png"
+  },
+  {
+    "revision": "9d084070ec4b058e3a6faa469732c5b9",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_Procurement.9d084070.png"
+  },
+  {
+    "revision": "8103d79d3f5c03db8a24ca614c2f2e9f",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Headers_QPS.8103d79d.png"
+  },
+  {
+    "revision": "b63c85fa6d9efc033d4feaa8e69bed87",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW-Group-Capability-Statement-2019.b63c85fa.pdf"
+  },
+  {
+    "revision": "c0cf9ca6c3836b4b159c31589a6286ba",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW-Group-Capability-Statement-2019.c0cf9ca6.jpg"
+  },
+  {
+    "revision": "6422cd008c0a4e4dbea542da5ac40377",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW-Group-Capability-Statement-20192.6422cd00.jpg"
+  },
+  {
+    "revision": "0dbe876806491382a986d8d2a450e5a3",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW-Group-Capability-Statement-20193.0dbe8768.jpg"
+  },
+  {
+    "revision": "5c7d2a088605cc62107e2836b55dc6e7",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW-Group-Capability-Statement-20194.5c7d2a08.jpg"
+  },
+  {
+    "revision": "56b94e1f2622fc223f21aaea55e133b9",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW-Group-Capability-Statement-20195.56b94e1f.jpg"
+  },
+  {
+    "revision": "b0c3844edbdd9c9c35c2286fa4a9a417",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW-Group-Capability-Statement-20196.b0c3844e.jpg"
+  },
+  {
+    "revision": "97ed988c63d50a5c5316ae2af2d7e646",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW-Group-Capability-Statement-20197.97ed988c.jpg"
+  },
+  {
+    "revision": "8df1f6fb612726f7718849fa632a9b6d",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW-Group-Capability-Statement-20198.8df1f6fb.jpg"
+  },
+  {
+    "revision": "2490cce2c56ce1b7d72423858017421c",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW-Group-Capability-Statement-20199.2490cce2.jpg"
+  },
+  {
+    "revision": "aafbc1070f6f54cd68735e6425b9112d",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKWFresh_Entry_Page_Images.aafbc107.png"
+  },
+  {
+    "revision": "fc7f100aa9760e814caa6e3fbee61a6e",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKWGroupOrgChart.fc7f100a.jpg"
+  },
+  {
+    "revision": "6a54cdb1aa7f544d4e32b1bc64be9b6f",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW_Header.6a54cdb1.png"
+  },
+  {
+    "revision": "b9194f5f9862c8cca268b26a64c91fb7",
+    "url": "/nkw-touchscreen-view-page-test/static/media/NKW_Logos_HVC.b9194f5f.png"
+  },
+  {
+    "revision": "96570bd71c20f25e97e1e82cf63edf83",
+    "url": "/nkw-touchscreen-view-page-test/static/media/PCS_Entry_Page_Images.96570bd7.png"
+  },
+  {
+    "revision": "8d0cf294dd790680642012dc0c27b8ee",
+    "url": "/nkw-touchscreen-view-page-test/static/media/PCS_Entry_Page_Images2.8d0cf294.png"
+  },
+  {
+    "revision": "b3af02ffc479dac7d49c6eca99955026",
+    "url": "/nkw-touchscreen-view-page-test/static/media/PCS_Entry_Page_Images3.b3af02ff.png"
+  },
+  {
+    "revision": "6b93c96eb8d8350386a096ccd1a7adc2",
+    "url": "/nkw-touchscreen-view-page-test/static/media/PCS_Entry_Page_Images4.6b93c96e.png"
+  },
+  {
+    "revision": "edc8f9f090a9d6e30ef7e9dfd766b04d",
+    "url": "/nkw-touchscreen-view-page-test/static/media/Procurement_Image.edc8f9f0.png"
+  },
+  {
+    "revision": "2585626ea7424b671c7bcd5801fc599d",
+    "url": "/nkw-touchscreen-view-page-test/static/media/QPS_Gallery_Images.2585626e.png"
+  },
+  {
+    "revision": "cc3b8a36efa4e4f86932526dbf9f403e",
+    "url": "/nkw-touchscreen-view-page-test/static/media/QPS_Gallery_Images10.cc3b8a36.png"
+  },
+  {
+    "revision": "07d54bf75c31fc115c9df045fe5310de",
+    "url": "/nkw-touchscreen-view-page-test/static/media/QPS_Gallery_Images2.07d54bf7.png"
+  },
+  {
+    "revision": "7e1f115762b5396dc2e7142cd4f63e76",
+    "url": "/nkw-touchscreen-view-page-test/static/media/QPS_Gallery_Images3.7e1f1157.png"
+  },
+  {
+    "revision": "a18a247a1b97ae091e17367e6450c4ad",
+    "url": "/nkw-touchscreen-view-page-test/static/media/QPS_Gallery_Images4.a18a247a.png"
+  },
+  {
+    "revision": "8a2c1749b45599f84f9aea0bea652b05",
+    "url": "/nkw-touchscreen-view-page-test/static/media/QPS_Gallery_Images5.8a2c1749.png"
+  },
+  {
+    "revision": "0b487c0875d5122ae3e7adc37afd46b2",
+    "url": "/nkw-touchscreen-view-page-test/static/media/QPS_Gallery_Images6.0b487c08.png"
+  },
+  {
+    "revision": "9bf338fcd686c345ae497240ae9e683c",
+    "url": "/nkw-touchscreen-view-page-test/static/media/QPS_Gallery_Images7.9bf338fc.png"
+  },
+  {
+    "revision": "83de8e893de3edf2ee75628cb521639f",
+    "url": "/nkw-touchscreen-view-page-test/static/media/QPS_Gallery_Images8.83de8e89.png"
+  },
+  {
+    "revision": "ec9469dc165ab28c57d5631adc6ecbae",
+    "url": "/nkw-touchscreen-view-page-test/static/media/QPS_Gallery_Images9.ec9469dc.png"
+  },
+  {
+    "revision": "a369c479abf9680a779c7d02a3f74ab4",
+    "url": "/nkw-touchscreen-view-page-test/static/media/camp_footer.a369c479.png"
+  },
+  {
+    "revision": "53757d9306796cd8c19df677d30a10c7",
+    "url": "/nkw-touchscreen-view-page-test/static/media/facebook-f-brands.53757d93.svg"
+  },
+  {
+    "revision": "ceacea0771802e82b638bfe2db67e51e",
+    "url": "/nkw-touchscreen-view-page-test/static/media/nkw_fresh_divistion_img_two.ceacea07.png"
+  },
+  {
+    "revision": "ce4945782ad57f367f6a25833df6dc15",
+    "url": "/nkw-touchscreen-view-page-test/static/media/procurement_footer.ce494578.png"
+  },
+  {
+    "revision": "ad64ba3319901ed0d08dddcd447f9484",
+    "url": "/nkw-touchscreen-view-page-test/static/media/qps_slider.ad64ba33.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/nkw-touchscreen-view-page-test/precache-manifest.c76d517d2034d0b508cde0f913dd612b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nkw-touchscreen-view-page-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
